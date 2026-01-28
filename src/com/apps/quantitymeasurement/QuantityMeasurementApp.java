f6d8d9ff9336f045bac1c9a9c460388b449bc28a
KE20670595 <keingale911@gmail.com>
2026-01-28 17:26:10 +0530

    UC2 code

--- src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,39 +1,52 @@
 package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
-    public static class Feet{
-        private final double value;
 
+    public static class Feet {
+
+        private final double value;
 
         public Feet(double value) {
             this.value = value;
         }
 
         @Override
-        public boolean equals(Object obj){
+        public boolean equals(Object obj) {
 
-            if(this==obj){
-                return true;
-            }
-            if(this==null){
-                return false;
-            }
-            if(!(obj instanceof Feet)){
-                return false;
-            }
+            if (this == obj) return true;
+            if (this == null) return false;
+            if (!(obj instanceof Feet)) return false;
+
             Feet other = (Feet) obj;
-            return Double.compare(this.value, other.value)==0;
+            return Double.compare(this.value, other.value) == 0;
         }
     }
-    public static class Inches{
 
+    public static class Inches {
+
+        private final double inch;
+
+        public Inches(double inch) {
+            this.inch = inch;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj) return true;
+            if (this == null) return false;
+            if (!(obj instanceof Inches)) return false;
+
+            Inches other = (Inches) obj;
+            return Double.compare(this.inch, other.inch) == 0;
+        }
     }
 
-    public static void main(String[] args) {
-        Feet f1=new Feet(2.5);
-        Feet f2=new Feet(1.5);
-        Feet f3=new Feet(2.5);
-        Inches inch=new Inches();
+    public static void demonstrateFeetEqulity() {
+        Feet f1 = new Feet(2.5);
+        Feet f2 = new Feet(1.5);
+        Feet f3 = new Feet(2.5);
+        Inches inch = new Inches(24.0);
 
         System.out.println(f1.equals(f3));
         System.out.println(f2.equals(f1));
@@ -41,5 +54,24 @@ public class QuantityMeasurementApp {
         System.out.println(f3.equals(null));
         System.out.println(f3.equals(f3));
         System.out.println(f3.equals(inch));
+    }
+
+    public static void demonstrateInchesEqulity() {
+        Inches f1 = new Inches(24.0);
+        Inches f2 = new Inches(11.5);
+        Inches f3 = new Inches(12.0);
+        Feet ft = new Feet(2.0);
+
+        System.out.println(f1.equals(f3));
+        System.out.println(f2.equals(f1));
+        System.out.println(f3.equals(f1));
+        System.out.println(f3.equals(null));
+        System.out.println(f3.equals(f3));
+        System.out.println(f3.equals(ft));
+    }
+
+    public static void main(String[] args) {
+        demonstrateInchesEqulity();
+        demonstrateFeetEqulity();
     }
 }
--- src/com/apps/quantitymeasurement/QuantitymeasurementAppTest.java
+++ src/com/apps/quantitymeasurement/QuantitymeasurementAppTest.java
@@ -7,40 +7,79 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class QuantitymeasurementAppTest {
 
+    //Test cases for feet
     @Test
-    public void testFeetEquality_SameValue(){
-        Feet f1=new Feet(1.5);
-        Feet f2=new Feet(1.5);
+    public void testFeetEquality_SameValue() {
+        Feet f1 = new Feet(1.5);
+        Feet f2 = new Feet(1.5);
 
-        assertEquals(f1,f2);
+        assertEquals(f1, f2);
     }
 
     @Test
-    public void testFeetEquality_DiffValue(){
-        Feet f1=new Feet(1.5);
-        Feet f2=new Feet(2.5);
+    public void testFeetEquality_DiffValue() {
+        Feet f1 = new Feet(1.5);
+        Feet f2 = new Feet(2.5);
 
-        assertNotEquals(f1,f2);
+        assertNotEquals(f1, f2);
     }
 
     @Test
-    public void testFeetEquality_NullValue(){
-        Feet f1=new Feet(1.5);
+    public void testFeetEquality_NullValue() {
+        Feet f1 = new Feet(1.5);
 
-        assertNotEquals(f1,null);
+        assertNotEquals(f1, null);
     }
 
     @Test
-    public void testFeetEquality_DiffObjValue(){
-        Feet f1=new Feet(1.5);
+    public void testFeetEquality_DiffObjValue() {
+        Feet f1 = new Feet(1.5);
 
-        assertNotEquals(f1, new Inches());
+        assertNotEquals(f1, new Inches(24.0));
     }
 
     @Test
-    public void testFeetEquality_SameRefValue(){
-        Feet f1=new Feet(1.5);
+    public void testFeetEquality_SameRefValue() {
+        Feet f1 = new Feet(1.5);
 
-        assertEquals(f1,f1);
+        assertEquals(f1, f1);
+    }
+
+    //Test cases for Inches
+    @Test
+    public void testInchesEquality_SameValue() {
+        Inches in1 = new Inches(12.0);
+        Inches in2 = new Inches(12.0);
+
+        assertEquals(in1, in2);
+    }
+
+    @Test
+    public void testInchesEquality_DiffValue() {
+        Inches in1 = new Inches(12.0);
+        Inches in2 = new Inches(24.0);
+
+        assertNotEquals(in1, in2);
+    }
+
+    @Test
+    public void testInchesEquality_NullValue() {
+        Inches in1 = new Inches(12.0);
+
+        assertNotEquals(in1, null);
+    }
+
+    @Test
+    public void testInchesEquality_DiffObjValue() {
+        Inches in1 = new Inches(12.0);
+
+        assertNotEquals(in1, new Feet(2.0));
+    }
+
+    @Test
+    public void testInchesEquality_SameRefValue() {
+        Inches in1 = new Inches(12.0);
+
+        assertEquals(in1, in1);
     }
 }
